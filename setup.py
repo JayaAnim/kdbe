edbc61549aa0975ecfb80dabd654fc19efcf70ae
Kurtis Jensen <k@kbuilds.com>
2018-09-22 16:12:30 -0400

    updates version in package. wow, that was out of date

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="kbde",
-      version="2",
+      version="5",
       url="https://gitlab.com/kbGit/kbde",
       author="kbuilds, LLC",
       author_email="k@kbuilds.com",
