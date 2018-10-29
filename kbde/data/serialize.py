c4d3733a2b0b731e68a9dedb5b5fe171fd6d0268
Kurtis Jensen <k@kbuilds.com>
2018-10-28 21:37:40 -0400

    fixes serialize field

--- kbde/data/serialize.py
+++ kbde/data/serialize.py
@@ -8,6 +8,6 @@ class Serializable:
         Returns a serializable version of self
 
         By default, returns the values and keys associated with
-        self.SERIALIZE_FIELDS
+        self.serialize_fields
         """
-        return {key:getattr(self,key) for key in self.SERIALIZE_FIELDS}
+        return {key:getattr(self, key) for key in self.serialize_fields}
