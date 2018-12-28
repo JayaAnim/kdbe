c172be5490d2b7c24d78b19d8c73d3870c30b43c
Kurtis Jensen <k@kbuilds.com>
2018-12-28 14:08:05 -0500

    adds a switch for username taking email

--- kbde/django/models.py
+++ kbde/django/models.py
@@ -5,10 +5,12 @@ MAX_LENGTH_CHAR_FIELD = 255
 
 
 class User(AbstractUser):
+    username_is_email = True
 
     class Meta:
         abstract = True
 
     def save(self, *args, **kwargs):
-        self.username = self.email
+        if self.username_is_email:
+            self.username = self.email
         super().save(*args, **kwargs)
