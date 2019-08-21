cea6add31ca6d347f8e49be31c3248aa3f73c0e3
Aaron Perkins <a2@obuilds.com>
2019-08-21 18:15:44 +0000

    adds ios orientation exif removal to image.py

--- kbde/cv/image.py
+++ kbde/cv/image.py
@@ -3,7 +3,8 @@ from imutils import paths
 
 import argparse
 import cv2
-import io
+import PIL
+import piexif
 import numpy as np
 import os, io
 
@@ -83,6 +84,49 @@ class Image():
         #compute the focus measure of the image using Variance of Laplacian method
         fm = cv2.Laplacian(im, cv2.CV_64F).var()
         return fm
+
+    def remove_ios_orientation_exif(self):
+        """
+        Checks self.image exif data for iOS orientation data
+        If found: removes from image, returns `True`
+        Else: does nothing, returns false
+        """
+        img = PIL.Image.open(self.image)
+        if "exif" in img.info:
+            exif_dict = piexif.load(img.info['exif'])
+
+            if piexif.ImageIFD.Orientation in exif_dict["0th"]:
+                img_format = img.format
+
+                orientation = exif_dict["0th"][piexif.ImageIFD.Orientation]
+
+                if orientation == 1:
+                    return False
+                elif orientation == 2:
+                    img = img.transpose(Image.FLIP_LEFT_RIGHT)
+                elif orientation == 3:
+                    img = img.rotate(180)
+                elif orientation == 4:
+                    img = img.rotate(180).transpose(Image.FLIP_LEFT_RIGHT)
+                elif orientation == 5:
+                    img = img.rotate(-90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+                elif orientation == 6:
+                    img = img.rotate(-90, expand=True)
+                elif orientation == 7:
+                    img = img.rotate(90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+                elif orientation == 8:
+                    img = img.rotate(90, expand=True)
+
+                exif_dict["0th"][piexif.ImageIFD.Orientation] = 1
+                exif_bytes = piexif.dump(exif_dict)
+
+                output_file = io.BytesIO()
+                img.save(output_file, format=img_format, exif=exif_bytes)
+
+                self.image = output_file
+                return True
+
+        return False
 
 
 class ImageBox:
