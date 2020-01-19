438450e5b5c797132c79e204843e6ed463837d5d
Kurtis Jensen <k@kbuilds.com>
2020-01-19 04:49:43 +0000

    makes settings an absolute override. all settings addressed in kbde settings will now need to be overridden again. fixes #2

--- kbde/django/settings.py
+++ kbde/django/settings.py
@@ -1,10 +1,8 @@
 from django import urls
+
 from config.settings_base import *
 
 import os
-
-
-config = globals()
 
 
 # App name
@@ -44,18 +42,18 @@ except ImportError:
 
 # Auth
 
-AUTH_USER_MODEL = config.get("AUTH_USER_MODEL", "user.User")
-LOGIN_REDIRECT_URL = config.get("LOGIN_REDIRECT_URL", "/")
-LOGOUT_REDIRECT_URL = config.get("LOGOUT_REDIRECT_URL", "/")
+AUTH_USER_MODEL = "user.User"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 
 # Media storage config
 
-AWS_ACCESS_KEY_ID = config.get("AWS_ACCESS_KEY_ID", os.getenv("AWS_ACCESS_KEY_ID"))
-AWS_SECRET_ACCESS_KEY = config.get("AWS_SECRET_ACCESS_KEY", os.getenv("AWS_SECRET_ACCESS_KEY"))
-AWS_STORAGE_BUCKET_NAME = config.get("AWS_STORAGE_BUCKET_NAME", APP_NAME)
-AWS_S3_ENDPOINT_URL = config.get("AWS_S3_ENDPOINT_URL", os.getenv("AWS_S3_ENDPOINT_URL"))
-AWS_QUERYSTRING_AUTH = config.get("AWS_QUERYSTRING_AUTH", False)
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = APP_NAME
+AWS_S3_ENDPOINT_URL = os.getenv("AWS_S3_ENDPOINT_URL")
+AWS_QUERYSTRING_AUTH = False
 
 try:
     import storages
@@ -64,9 +62,8 @@ try:
         "storages",
         ]
 
-    DEFAULT_FILE_STORAGE = config.get("DEFAULT_FILE_STORAGE",
-                                      "kbde.django.storage_backends.MediaStorage")
-    MEDIA_URL = config.get("MEDIA_URL", "{}/{}/".format(AWS_S3_ENDPOINT_URL, "media"))
+    DEFAULT_FILE_STORAGE = "kbde.django.storage_backends.MediaStorage"
+    MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/media/"
 
 except ImportError:
     pass
@@ -83,69 +80,64 @@ DEBUG_EMAIL = os.getenv("DEBUG_EMAIL")
 
 # Logging
 
-LOGGING = config.get("LOGGING")
 APP_LOG_LEVEL = os.getenv("APP_LOG_LEVEL", "INFO")
 
-if LOGGING is None:
-    LOGGING = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "filters": {
-            "require_debug_false": {
-                "()": "django.utils.log.RequireDebugFalse",
-                },
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
             },
-        "formatters": {
-            "verbose": {
-                "format": ("[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s"),
-                "datefmt": "%Y-%m-%d %H:%M:%S",
-                },
+        },
+    "formatters": {
+        "verbose": {
+            "format": ("[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s"),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
             },
-        "handlers": {
-            "console": {
-                "level": "DEBUG",
-                "class": "logging.StreamHandler",
-                "formatter": "verbose",
-                },
-            "mail_admins": {
-                "level": "ERROR",
-                "filters": ["require_debug_false"],
-                "class": "django.utils.log.AdminEmailHandler",
-                },
+        },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
             },
-        "loggers": {
-            "django": {
-                "handlers": ["console", "mail_admins"],
-                "level": APP_LOG_LEVEL,
-                },
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
             },
-        }
+        },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "mail_admins"],
+            "level": APP_LOG_LEVEL,
+            },
+        },
+    }
 
 
 # Email
 
-EMAIL_HOST = config.get("EMAIL_HOST", os.getenv("EMAIL_HOST", "smtp.sendgrid.net"))
-EMAIL_PORT = config.get("EMAIL_PORT", os.getenv("EMAIL_PORT", "587"))
-EMAIL_USE_TLS = config.get("EMAIL_USE_TLS", True)
-EMAIL_HOST_USER = config.get("EMAIL_HOST_USER", os.getenv("EMAIL_USERNAME", "apikey"))
-EMAIL_HOST_PASSWORD = config.get("EMAIL_HOST_PASSWORD", os.getenv("EMAIL_PASSWORD"))
-EMAIL_SUBJECT_PREFIX = "[{}] ".format(config.get("EMAIL_SUBJECT_PREFIX", APP_NAME))
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.sendgrid.net")
+EMAIL_PORT = os.getenv("EMAIL_PORT", "587")
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_USERNAME", "apikey")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_SUBJECT_PREFIX = f"[{APP_NAME}] "
 
 
 # Staticfiles config
 
-STATIC_URL = config.get("STATIC_URL", '/static/')
-STATIC_ROOT = config.get("STATIC_ROOT", os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # SSL
 
-SECURE_PROXY_SSL_HEADER = config.get("SECURE_PROXY_SSL_HEADER")
-
-if SECURE_PROXY_SSL_HEADER is None:
-    SECURE_PROXY_SSL_HEADER = os.getenv("SECURE_PROXY_SSL_HEADER")
-    if SECURE_PROXY_SSL_HEADER is not None:
-        SECURE_PROXY_SSL_HEADER = SECURE_PROXY_SSL_HEADER.split(":")
+SECURE_PROXY_SSL_HEADER = os.getenv("SECURE_PROXY_SSL_HEADER")
+if SECURE_PROXY_SSL_HEADER is not None:
+    SECURE_PROXY_SSL_HEADER = SECURE_PROXY_SSL_HEADER.split(":")
 
 if SECURE_PROXY_SSL_HEADER:
     SECURE_SSL_REDIRECT = True
