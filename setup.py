705742d117d019c6ebe613ae86098eb26a25f195
Kurtis Jensen <k@kbuilds.com>
2020-02-01 00:06:35 +0000

    updates readme and settings

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="kbde",
-      version="29",
+      version="30",
       url="https://gitlab.com/kb_git/kbde",
       author="kbuilds, LLC",
       author_email="k@kbuilds.com",
