b79924bede349f3e9cdbb5256b529a4793cac1c7
Kurtis Jensen <k@kbuilds.com>
2020-03-21 15:55:59 +0000

    adds support for the default password from settings

--- kbde/django/management/commands/create_admin_superusers.py
+++ kbde/django/management/commands/create_admin_superusers.py
@@ -10,11 +10,16 @@ class Command(management.base.BaseCommand):
     def handle(self, *args, **options):
         User = auth.get_user_model()
 
+        assert settings.ADMIN_DEFAULT_PASSWORD is not None, "must set `settings.ADMIN_DEFAULT_PASSWORD` to run this command"
+
         for name, email in settings.ADMINS:
             # Get or create a user with the given email
             user_fields = {User.USERNAME_FIELD: email}
 
             user, created = User.objects.get_or_create(**user_fields)
+
+            if created:
+                user.set_password(settings.ADMIN_DEFAULT_PASSWORD)
 
             user.is_superuser = True
             user.is_staff = True
