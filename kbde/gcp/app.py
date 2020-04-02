9d64d7ab467ce85e3fa823293facbcbbad932e6b
Kurtis Jensen <k@kbuilds.com>
2020-04-02 05:21:21 +0000

    adds gcloud app commands for managing versions

--- kbde/gcp/app.py
+++ kbde/gcp/app.py
@@ -8,3 +8,9 @@ class App:
 
     def deploy(self, *args, **kwargs):
         return self.gcloud.run("app", "deploy", *args, **kwargs)
+
+    def list_versions(self, *args, **kwargs):
+        return self.gcloud.run("app", "versions", "list", *args, **kwargs)
+
+    def delete_version(self, service, version_id):
+        return self.gcloud.run("app", "versions", "delete", version_id, service=service, quiet="")
