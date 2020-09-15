6030c9b20e35185b2bee36870573bcd43cf06f81
Kurtis Jensen <k@kbuilds.com>
2020-09-15 21:20:57 +0000

    adds python and base installer

--- kbde/install/python.py
+++ kbde/install/python.py
@@ -3,13 +3,6 @@ from . import apt_installer
 
 class Installer(apt_installer.Installer):
     package_names = [
-        "tree",
-        "parallel",
-        "gettext",
-        "vim",
-        "git",
-        "wget",
-        "htop",
         "build-essential",
         "python3-dev",
         "python3-venv",
