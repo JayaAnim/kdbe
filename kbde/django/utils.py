9613f4b96e088e83a6363a5220b7c6d8ff266356
Kurtis Jensen <k@kbuilds.com>
2020-09-22 16:45:58 +0000

    adds send_sms and validation functions

--- kbde/django/utils.py
+++ kbde/django/utils.py
@@ -26,14 +26,22 @@ def send_email(to_email_list,
     """
 
     # Make sure that the DEBUG_EMAIL setting is present
-    assert hasattr(settings, "DEBUG_EMAIL"), "must set `DEBUG_EMAIL` in settings"
+    assert (
+        hasattr(settings, "DEBUG_EMAIL"),
+        "must set `DEBUG_EMAIL` in settings"
+    )
     
     # If in DEBUG mode, make sure that DEBUG_EMAIL is set to something
     if settings.DEBUG:
-        assert settings.DEBUG_EMAIL, "Must set `DEBUG_EMAIL` when in DEBUG mode"
+        assert (
+            settings.DEBUG_EMAIL,
+            "Must set `DEBUG_EMAIL` when in DEBUG mode",
+        )
 
-    assert text_message is not None or html_message is not None, ("must pass `text_message` or "
-                                                                  "`html_message`")
+    assert (
+        text_message is not None or html_message is not None,
+        "must pass `text_message` or `html_message`",
+    )
 
     message = mail.EmailMultiAlternatives()
 
@@ -74,6 +82,115 @@ def send_email(to_email_list,
     return message
 
 
+def send_sms(to_phone_number, message, from_phone_number=None):
+    """
+    Sends SMS wit a "debugged_phone_number"
+    """
+    import twilio
+
+    client = twilio.rest.Client(
+        settings.TWILIO_ACCOUNT_SID,
+        settings.TWILIO_AUTH_TOKEN,
+    )
+
+    to_phone_number = get_debugged_phone_number(to_phone_number)
+
+    from_phone_number = (
+        from_phone_number or
+        getattr(settings, "DEFAULT_SMS_FROM_PHONE_NUMBER", None)
+    )
+
+    assert (
+        from_phone_number,
+        ("You must either pass `from_phone_number` to this function or set "
+         "settings.DEFAULT_SMS_FROM_PHONE_NUMBER"),
+    )
+
+    message = client.messages.create(
+        from_=from_phone_number,
+        body=message,
+        to=to_phone_number,
+    )
+
+    return message
+
+
+def send_sms_verificaton(phone_number):
+    """
+    Uses the Twilio verification API to send a code to a phone_number
+    https://www.twilio.com/verify/api
+    """
+    import twilio
+
+    client = twilio.rest.Client(
+        settings.TWILIO_ACCOUNT_SID,
+        settings.TWILIO_AUTH_TOKEN,
+    )
+
+    phone_number = get_debugged_phone_number(phone_number)
+
+    verification = client.verify.services(
+        settings.TWILIO_VERIFICATION_SERVICE_SID,
+    ).verifications.create(
+        to=phone_number,
+        channel="sms",
+    )
+
+    return verification
+
+
+def verify_sms_verification(phone_number, code):
+    """
+    Checks a verification code against the Twilio verificaton API
+    """
+    import twilio
+
+    client = twilio.rest.Client(
+        settings.TWILIO_ACCOUNT_SID,
+        settings.TWILIO_AUTH_TOKEN,
+    )
+
+    phone_number = get_debugged_phone_number(phone_number)
+
+    verificaton_check = client.verify.services(
+        settings.TWILIO_VERIFICATION_SERVICE_SID,
+    ).verification_checks.create(
+        to=phone_number,
+        code=code,
+    )
+
+    return verificaton_check
+
+
+def get_debugged_phone_number(phone_number):
+    """
+    Takes a phone_number
+    Returns the phone_number or the DEBUG_PHONE_NUMBER from settings
+    """
+    assert isinstance(phone_number, str), "`phone_number` must be a string"
+
+    assert (
+        hasattr(settings, "DEBUG_PHONE_NUMBER"),
+        "must set `DEBUG_PHONE_NUMBER` in settings"
+    )
+
+    # If in DEBUG mode, make sure that DEBUG_PHONE_NUMBER is set to something
+    if settings.DEBUG:
+        assert (
+            settings.DEBUG_PHONE_NUMBER,
+            "Must set `DEBUG_PHONE_NUMBER` when in DEBUG mode",
+        )
+
+    if settings.DEBUG_PHONE_NUMBER:
+        assert (
+            isinstance(settings.DEBUG_PHONE_NUMBER, str),
+            "`settings.DEBUG_PHONE_NUMBER` must be a string",
+        )
+        phone_number = settings.DEBUG_PHONE_NUMBER
+
+    return phone_number
+
+
 def send_to_trello(board_email,
                    title,
                    description,
@@ -86,11 +203,17 @@ def send_to_trello(board_email,
     """
     # Make sure that users do not include the @ at the beginning
     if member_list:
-        assert not member_list[0].startswith("@"), "usernames should not include `@`"
+        assert (
+            not member_list[0].startswith("@"),
+            "usernames should not include `@`",
+        )
 
     # Make sure that labels to not include the # at the beginning
     if label_list:
-        assert not label_list[0].startswith("#"), "labels should not include `#`"
+        assert (
+            not label_list[0].startswith("#"),
+            "labels should not include `#`",
+        )
 
     # Create member and label lists
     member_list = ["@{}".format(username) for username in member_list]
