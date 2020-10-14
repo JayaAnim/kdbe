c3a2ae1da8e386ecc47038e940aaf8701c330062
Kurtis Jensen <k@kbuilds.com>
2020-10-14 01:29:07 +0000

    WIP: scrapers

--- kbde/scraper/scraper.py
+++ kbde/scraper/scraper.py
@@ -1,12 +1,17 @@
-import time, bs4
+import time, bs4, urllib
+
+from . import mixins
 
 
 class Base:
     page_ready_check_interval = 1
     
-    def __init__(self, auth_credentials=None):
+    def __init__(self, auth_credentials=None, start_page_url=None):
         self.auth_credentials = auth_credentials
+        self.start_page_url = start_page_url
+
         self.device = self.get_device()
+        self.page_stack = []
 
     def get_device(self):
         """
@@ -26,6 +31,9 @@ class Base:
 
             # Get the HTML for the current page
             page_html = self.get_page_html(self.device, page_url)
+            
+            # Save the url to the page_stack for this scraper
+            self.page_stack.append(page_url)
 
             for obj in self.get_data_from_page(page_html):
                 yield obj
@@ -47,7 +55,12 @@ class Base:
         """
         Retun the url of the starting page for scraping
         """
-        raise NotImplementedError
+        assert self.start_page_url, (
+            f"{self.__class__.__name__} must be instantiated with "
+             "`start_page_url` or override .get_start_page_url()"
+        )
+
+        return self.start_page_url
 
     def get_page_html(self, device, url):
         """
@@ -68,16 +81,27 @@ class Base:
         """
         raise NotImplementedError
 
+    def get_start_page_host(self):
+        return self.get_host_from_url(self.start_page_url)
 
-class Static(Base):
+    def get_current_host(self):
+        return self.get_host_from_url(self.get_current_url())
+        
+    def get_host_from_url(self, url):
+        parsed_url = urllib.parse.urlparse(url)
+        return f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+    def get_current_url(self):
+        if not self.page_stack:
+            return None
+        else:
+            return self.page_stack[-1]
+
+
+class Static(mixins.RequestsDevice, Base):
     """
     Scraper which processes static HTML web pages
     """
-    
-    def get_device(self):
-        import requests
-
-        return requests.Session()
 
     def login(self, device, auth_credentials):
         """
@@ -88,13 +112,11 @@ class Static(Base):
         if auth_credentials is None:
             return None
 
-        assert (
-            isinstance(auth_credentials, tuple),
-            "auth_credentials must be a tuple",
+        assert isinstance(auth_credentials, tuple), (
+            "auth_credentials must be a tuple"
         )
-        assert (
-            len(auth_credentials) == 2,
-            "auth_credentials must have a length of 2: (username, password)",
+        assert len(auth_credentials) == 2, (
+            "auth_credentials must have a length of 2: (username, password)"
         )
 
         device.auth = auth_credentials
@@ -105,3 +127,16 @@ class Static(Base):
         assert response.status_code == 200
 
         return bs4.BeautifulSoup(response.text, features="html.parser")
+
+
+class JsonApi(mixins.RequestsDevice, Base):
+    """
+    Scraper to process json from an API endpoint
+    """
+
+    def get_page_html(self, device, url):
+        response = device.get(url)
+
+        assert response.status_code == 200
+
+        return response.json()
