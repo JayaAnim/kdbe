a5d872206c6fb05eacbb774f47ce6e12ea8684aa
Kurtis Jensen <k@kbuilds.com>
2021-01-18 20:22:24 +0000

    moves to pascal case path names and template file names

--- kbde/django/urls.py
+++ kbde/django/urls.py
@@ -8,16 +8,12 @@ def path(url_path, view_class):
     Returns a complete Django path based on just the view_class.
     Overrides template_name based on class name.
     """
-    # Break view class name into pieces
-    split_class_name = [
-        s for s in re.split("([A-Z][^A-Z]*)", view_class.__name__) if s
-    ]
-
-    # Path name is the view name in snake case
-    path_name = "_".join(s.lower() for s in split_class_name)
+    # Path name is the view name
+    path_name = view_class.__name__
 
     # Template name is {module_name}/{path_name}.html
-    module_name = view_class.__module__.split(".")[0]
+    module_name_list = view_class.__module__.split(".")[:-1]
+    module_name = "/".join(module_name_list)
     template_name = f"{module_name}/{path_name}.html"
 
     if hasattr(view_class, "template_name"):
