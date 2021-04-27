0ab2be98244ae25bb3fa9da852fa7fa3804ec16e
Kurtis Jensen <k@kbuilds.com>
2021-04-27 21:14:59 +0000

    adds modal view

--- kbde/django/views/__init__.py
+++ kbde/django/views/__init__.py
@@ -126,6 +126,7 @@ class TableView(ListView):
     include_row_data = True
     paginate = True
     pages_to_show = 5
+    search_url_kwarg = None
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
@@ -160,6 +161,7 @@ class TableView(ListView):
             "empty_message": self.get_table_empty_message(),
             "paginate": self.get_paginate(),
             "pages_to_show": self.get_pages_to_show(),
+            "extra": self.get_extra(),
         }
 
     def get_label_list(self):
@@ -222,6 +224,17 @@ class TableView(ListView):
     def get_pages_to_show(self):
         return self.pages_to_show
 
+    def get_extra(self):
+        extra_kwargs = self.get_extra_kwargs()
+        extra_args = [f"{key}={value}" for key, value in extra_kwargs.items()]
+
+        return "&".join(extra_args)
+
+    def get_extra_kwargs(self):
+        extra_kwargs = {key: self.request.GET[key] for key in self.request.GET}
+        extra_kwargs.pop("page", None)
+        return extra_kwargs
+
 
 class SearchFormView(FormView):
     form_class = forms.SearchForm
@@ -231,3 +244,50 @@ class SearchFormView(FormView):
 
     def get_initial(self):
         return self.request.GET
+
+
+class ModalView(TemplateView):
+    template_name = "kbde/ModalView.html"
+    modal_button_text = None
+    default_modal_button_class = "btn btn-primary"
+    modal_header_text = None
+    modal_body_template_name = None
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+
+        context_data.update({
+            "modal_button_text": self.get_modal_button_text(),
+            "modal_button_class": self.get_modal_button_class(),
+            "modal_header_text": self.get_modal_header_text(),
+            "modal_body_template_name": self.get_modal_body_template_name(),
+        })
+
+        return context_data
+
+    def get_modal_button_text(self):
+        assert self.modal_button_text is not None, (
+            f"{self.__class__} must define .modal_button_text or override "
+            f".get_modal_button_text()"
+        )
+        return self.modal_button_text
+
+    def get_modal_button_class(self):
+        return self.kwargs.get(
+            "modal_button_class",
+            self.default_modal_button_class,
+        )
+
+    def get_modal_header_text(self):
+        assert self.modal_header_text is not None, (
+            f"{self.__class__} must define .modal_header_text or "
+            f"override .get_modal_header_text()"
+        )
+        return self.modal_header_text
+
+    def get_modal_body_template_name(self):
+        assert self.modal_body_template_name is not None, (
+            f"{self.__class__} must define .modal_body_template_name or "
+            f"override .get_modal_body_template_name()"
+        )
+        return self.modal_body_template_name
