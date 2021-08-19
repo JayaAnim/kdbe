4d769d60888fb6f0ddef4486a59f2bec099f20d2
Kurtis Jensen <k@kbuilds.com>
2021-08-19 22:14:01 +0000

    completed sass app

---
+++ kbde/django/sass/views.py
@@ -0,0 +1,23 @@
+from django.core import management
+from django.conf import settings
+from kbde.django import views as kbde_views
+
+from . import response
+
+
+class Page(kbde_views.TemplateView):
+    response_class = response.SassResponse
+    permission_classes = []
+    content_type = "text/css"
+
+    def get(self, *args, **kwargs):
+        if settings.DEBUG_CSS:
+            management.call_command("collectstatic", "--no-input")
+
+        return super().get(*args, **kwargs)
+
+    def get_template_names(self):
+        return [
+            "sass/page.sass",
+            "sass/page.scss",
+        ]
