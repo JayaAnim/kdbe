c80e907cb085fb0678eea96448a1bc47c224ec72
Kurtis Jensen <k@kbuilds.com>
2021-10-28 19:05:53 +0000

    pwa installer

--- kbde/django/pwa/templates/kbde/django/pwa/views/Install.js
+++ kbde/django/pwa/templates/kbde/django/pwa/views/Install.js
@@ -10,9 +10,14 @@ $manifest.attr("href", "{% url 'pwa:Manifest' %}")
 var $service_worker = $("<script>")
 $service_worker.attr("src", "{% url 'pwa:ServiceWorker' %}")
 
+var $apple_touch_icon = $("<link>")
+$apple_touch_icon.attr("rel", "apple-touch-icon")
+$apple_touch_icon.attr("href", "{{ manifest.icons.0.src }}")
+
 var $head = $("head")
 $head.append($manifest)
 $head.append($service_worker)
+$head.append($apple_touch_icon)
 
 
 if ("serviceWorker" in navigator) {
@@ -26,8 +31,21 @@ if ("serviceWorker" in navigator) {
 
 }
 
-self.addEventListener("appinstalled", function(){
+window.addEventListener("appinstalled", function(){
     window.location.href = "{{ manifest.start_url }}"
+})
+
+var deferred_prompt = null
+
+window.addEventListener('beforeinstallprompt', function(e){
+    e.preventDefault()
+    deferred_prompt = e
+    console.log("defer")
+})
+
+$(".pwa-install-button").click(function(){
+    deferred_prompt.prompt()
+    deferred_prompt = null
 })
 
 })
