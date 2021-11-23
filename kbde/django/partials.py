a97a51ca452fa146197f05f2d7cdfd8b2472f1c9
Aaron Perkins <a2@obuilds.com>
2021-11-23 22:27:51 +0000

    makes form fields use template partials instead of view partials

--- kbde/django/partials.py
+++ kbde/django/partials.py
@@ -1,14 +1,2 @@
 from . import views
 
-
-class FormFieldPartial(views.TemplateView):
-    template_name = 'kbde/django/partials/FormField.html'
-    permission_classes = []
-
-    def get_context_data(self, *args, **kwargs):
-        data = super().get_context_data(*args, **kwargs)
-
-        data['field'] = self.kwargs['field']
-
-        return data
-
