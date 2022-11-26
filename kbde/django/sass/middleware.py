f7caef9c078d1f36425f63e8a2330256c8fe9914
Kurtis Jensen <k@kbuilds.com>
2022-11-26 05:27:00 +0000

    removes bs4 for sass

--- kbde/django/sass/middleware.py
+++ kbde/django/sass/middleware.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.core import management
+from django.utils import encoding, html
 
-import bs4, sass
+import sass
 
 
 class EmbeddedSassMiddleware:
@@ -20,6 +21,8 @@ class EmbeddedSassMiddleware:
         "EMBEDDED_SASS_CACHE_TIMEOUT",
         60*60*24,  # 24 hours
     )
+    opening_style_tag = "<style sass>"
+    ending_style_tag = "</style>"
 
     def __init__(self, get_response):
         self.get_response = get_response
@@ -32,30 +35,56 @@ class EmbeddedSassMiddleware:
         if "text/html" not in response.get("content-type", "").lower():
             return response
 
-        document = bs4.BeautifulSoup(response.content, features="html.parser")
+        content = encoding.smart_str(response.content)
 
-        if document.html == None:
-            # There is no html element in this document. Assume it is a partial
-            # Remove all style tags
-            style_tags = document.find_all("style", {"sass": True})
-            
-            for style_tag in style_tags:
-                style_tag.extract()
+        content, style_tags = self.extract_style_tags(content)
 
-        else:
+        if "<html>" in content:
             # This is a full-page document
             # Process all sass style tags
-            sass_document = self.get_sass(document)
+            sass_document = self.get_sass(style_tags)
 
             if sass_document:
-                css_style_tag = document.new_tag("style")
-                css_style_tag.string = self.get_css(sass_document)
-                document.html.head.append(css_style_tag)
+                css_document = self.get_css(sass_document)
 
-        response.content = str(document)
+                style_tag = f"<style>{css_document}</style>"
+                insertion_point = "</head>"
+
+                content = content.replace(
+                    insertion_point,
+                    style_tag + insertion_point,
+                )
+
+        response.content = str(content)
         response['content-length'] = str(len(response.content))
 
         return response
+
+    def extract_style_tags(self, content):
+        style_tags = []
+
+        split_content = content.split(self.opening_style_tag)
+
+        if len(split_content) == 1:
+            # There are no style tags
+            return content, style_tags
+
+        content = [split_content[0]]
+
+        for content_part in split_content[1:]:
+            split_content_part = content_part.split(self.ending_style_tag)
+
+            style_tag = split_content_part[0]
+            other_content = split_content_part[1:]
+
+            content_part = self.ending_style_tag.join(other_content)
+
+            style_tags.append(style_tag)
+            content.append(content_part)
+
+        content = "".join(content)
+
+        return content, style_tags
 
     def get_css(self, sass_document):
 
@@ -73,15 +102,11 @@ class EmbeddedSassMiddleware:
 
         return compile_sass(sass_document)
 
-    def get_sass(self, document):
-        style_tags = document.find_all("style", {"sass": True})
-
+    def get_sass(self, style_tags):
         sass_strings = []
 
         for style_tag in style_tags:
-            if style_tag.string not in sass_strings:
-                sass_strings.append(style_tag.string)
-
-            style_tag.extract()
+            if style_tag not in sass_strings:
+                sass_strings.append(style_tag)
 
         return "\n\n".join(sass_strings)
