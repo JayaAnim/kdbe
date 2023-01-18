c0b269174be02e833a1f14a9929a38d7771922ca
Kurtis Jensen <k@kbuilds.com>
2023-01-18 00:11:55 +0000

    improvements to the has_permissions template tag

--- kbde/django/templatetags/kbde.py
+++ kbde/django/templatetags/kbde.py
@@ -1,4 +1,4 @@
-from django import template
+from django import template, urls, http
 from django.core import exceptions
 from kbde.import_tools import utils as import_utils
 
@@ -7,14 +7,41 @@ register = template.Library()
 
 
 @register.filter()
-def has_permissions(request, view_name):
-    view_class = import_utils.import_class_from_string(view_name)
+def has_permissions(request, view_location):
+    """
+    Takes a request and view_location.
+    The view_location can be a full url, or a path to a view.
+    If a URL can not be resolved from view_location, then the view location
+    will be used to import the view_class
+    """
+    view = None
 
-    view = view_class()
-    view.setup(request)
+    try:
+        # Try resolving the url
+        resolver_match = urls.resolve(view_location)
+        view_class = resolver_match.func.view_class
+        view = view_class()
+        view.setup(request, *resolver_match.args, **resolver_match.kwargs)
+
+    except urls.exceptions.Resolver404:
+
+        try:
+            # Try importing the url by the view name
+            view_class = import_utils.import_class_from_string(view_location)
+            view = view_class()
+            view.setup(request)
+        except import_utils.ImportUtilsException:
+            pass
+
+    assert view is not None, (
+        f"Could not find view for \"{view_location}\". This must be either a "
+        f"resolvable url, or a path to a view (i.e. my_module.MyViewClass)"
+    )
 
     try:
         view.check_permissions()
+    except http.Http404:
+        assert False, f"Permission check returned 404 for {view_location}"
     except exceptions.PermissionDenied:
         return False
 
--- kbde/import_tools/utils.py
+++ kbde/import_tools/utils.py
@@ -1,9 +1,20 @@
 import importlib
 
 
+class ImportUtilsException(Exception):
+    pass
+
+
 def import_class_from_string(class_string):
     split_class_string = class_string.split(".")
     module_string = ".".join(split_class_string[:-1])
+
+    if not module_string:
+        raise ImportUtilsException(
+            f"The given string to import, {class_string}, did not contain a "
+            f"module and a class, i.e. my_module.MyClass"
+        )
+
     module = importlib.import_module(module_string)
 
     return getattr(module, split_class_string[-1])
