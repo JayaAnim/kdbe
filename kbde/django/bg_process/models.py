750e82cb00c5f5958a5cf2e7a68b450e6b2b0df2
Kurtis Jensen <k@kbuilds.com>
2023-03-27 19:35:05 +0000

    improvements to the bg process model

--- kbde/django/bg_process/models.py
+++ kbde/django/bg_process/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-from kbde.django import (models as kbde_models,
-                         rq as kbde_rq)
+from kbde.django import (
+    models as kbde_models,
+    rq as kbde_rq,
+)
 
-import uuid
+import uuid, traceback
 
 
 class AbstractBgProcess(models.Model):
@@ -20,11 +22,16 @@ class AbstractBgProcess(models.Model):
     )
 
     bg_process_queue_name = "default"
+    bg_process_failure_ttl = 60 * 60 * 10  # 10 minutes
+    bg_process_refresh_from_db = True
 
     slug = models.UUIDField(default=uuid.uuid4)
-    bg_process_status = models.CharField(max_length=kbde_models.MAX_LENGTH_CHAR_FIELD,
-                                         choices=BG_PROCESS_STATUS_CHOICES,
-                                         default=BG_PROCESS_STATUS_NEW)
+    bg_process_status = models.CharField(
+        max_length=kbde_models.MAX_LENGTH_CHAR_FIELD,
+        choices=BG_PROCESS_STATUS_CHOICES,
+        default=BG_PROCESS_STATUS_NEW,
+    )
+    bg_process_exception = models.TextField(blank=True)
 
     class Meta:
         abstract = True
@@ -33,30 +40,52 @@ class AbstractBgProcess(models.Model):
         return f"{super().__str__()} - {self.get_bg_process_status_display()}"
 
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+        result = super().save(*args, **kwargs)
 
         if self.bg_process_status == self.BG_PROCESS_STATUS_NEW:
             # Queue the processing
             self.queue_bg_process()
 
+        return result
+
     def queue_bg_process(self):
         self.bg_process_status = self.BG_PROCESS_STATUS_PENDING
-        self.save()
+        self.bg_process_exception = ""
+        self.save(
+            update_fields=[
+                "bg_process_status",
+                "bg_process_exception",
+            ],
+        )
 
-        return kbde_rq.queue_task(self.bg_process_queue_name, self.run_bg_process)
+        return kbde_rq.queue_task(
+            self.bg_process_queue_name,
+            self.run_bg_process,
+            failure_ttl=self.bg_process_failure_ttl,
+        )
 
     def run_bg_process(self):
+
+        if self.bg_process_refresh_from_db:
+            self.refresh_from_db()
+
         self.bg_process_status = self.BG_PROCESS_STATUS_PROCESSING
-        self.save()
+        self.save(update_fields=["bg_process_status"])
 
         try:
             self.bg_process()
             self.bg_process_status = self.BG_PROCESS_STATUS_COMPLETED
-            self.save()
+            self.save(update_fields=["bg_process_status"])
 
         except Exception as e:
             self.bg_process_status = self.BG_PROCESS_STATUS_FAILED
-            self.save()
+            self.bg_process_exception = traceback.format_exc()
+            self.save(
+                update_fields=[
+                    "bg_process_status",
+                    "bg_process_exception",
+                ],
+            )
             raise
 
     def bg_process(self):
@@ -64,7 +93,9 @@ class AbstractBgProcess(models.Model):
         This method runs async
         Override to do work
         """
-        raise NotImplementedError(f"{self.__class__.__name__} must implement self.bg_process()")
+        raise NotImplementedError(
+            f"{self.__class__} must implement self.bg_process()",
+        )
 
 
 class BgProcessModel(AbstractBgProcess):
